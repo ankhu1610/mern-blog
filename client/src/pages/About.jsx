72c3f2d0da7d87ce673fd09eda7718141f01f710
Ankit <chaubeyankit837@gmail.com>
2024-05-06 16:51:20 +0530

    add pre installation

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,32 @@
-
-const About = () => {
+export default function About() {
     return (
-        <div>
-            about
+      <div className='min-h-screen flex items-center justify-center'>
+        <div className='max-w-2xl mx-auto p-3 text-center'>
+          <div>
+            <h1 className='text-3xl font font-semibold text-center my-7'>
+              About YourBlog
+            </h1>
+            <div className='text-md text-gray-500 flex flex-col gap-6'>
+              <p>
+              Welcome to our blog, where we delve into the intricate tapestry of human existence. Here, we celebrate the diversity of perspectives, experiences,
+               and narratives that make up our world. From the profound to the mundane, the extraordinary to the everyday, our blog is a space for exploration, reflection, and connection.
+              </p>
+  
+              <p>
+              Join us as we navigate the complexities of life through the power of words.
+              Through insightful articles, thought-provoking essays, and engaging discussions, we aim to shine a light on the myriad facets of the human condition.
+              </p>
+  
+              <p>
+                We encourage you to leave comments on our posts and engage with
+                other readers. You can like other people's comments and reply to
+                them as well. We believe that a community of learners can help
+                each other grow and improve.
+              </p>
+            </div>
+          </div>
         </div>
+      </div>
     );
-};
-
-export default About;
+  }
+  
